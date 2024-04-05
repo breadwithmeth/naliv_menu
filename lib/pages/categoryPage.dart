4404fb031629ee482db750b3b80ff8c5606452b2
Twine <64853635+Twelvet-Spark@users.noreply.github.com>
2024-04-05 14:19:19 +0500

    ++

---
+++ lib/pages/categoryPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// Uri.base.queryParametersAll
+
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Placeholder(),
+    );
+  }
+}
